0c54aa73fa1887bd51fdf5f7d9d959fbfbf5d7ea
Ajay Kumar <ajay@primepenguin.com>
2022-06-29 18:01:50 +0530

    Added bundle line item information in orders

--- QuickbutikSharp/Entities/LineItem.cs
+++ QuickbutikSharp/Entities/LineItem.cs
@@ -17,5 +17,38 @@ namespace QuickbutikSharp.Entities
         [JsonProperty("variant_id")] public string VariantId { get; set; }
 
         [JsonProperty("sku")] public string Sku { get; set; }
+
+        [JsonProperty("app")]
+        public App App { get; set; }
+    }
+
+    public partial class App
+    {
+        [JsonProperty("productpackages")]
+        public Productpackage[] Productpackages { get; set; }
+    }
+
+    public partial class Productpackage
+    {
+        [JsonProperty("product_id")]
+        public string ProductId { get; set; }
+
+        [JsonProperty("variant_id")]
+        public string VariantId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("variant")]
+        public string Variant { get; set; }
+
+        [JsonProperty("qty")]
+        public string Qty { get; set; }
+
+        [JsonProperty("sku")]
+        public string Sku { get; set; }
+
+        [JsonProperty("price")]
+        public string Price { get; set; }
     }
 }
